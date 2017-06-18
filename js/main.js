f2b8cb09843a9597fcfd2e3bdfe9472b3ecf97e2
Ailton Ferreira <ailtonrkr@hotmail.com>
2017-06-18 19:13:36 -0300

    finalizado

--- js/main.js
+++ js/main.js
@@ -5,4 +5,24 @@ $('.owl-carousel').owlCarousel({
     autoplay: true,
     autoplayTimeout: 3000
    
+});
+
+//Efeito scroll animado
+$("ul.navbar-nav > li > a").on("click", function(event){
+	event.preventDefault();
+
+	$('ul.navbar-nav li').removeClass('active');
+	$(this).parent().addClass('active');
+
+	$("html, body").animate({scrollTop: $(this.hash).offset().top-60}, 1200);
+});
+
+$(window).scroll(function(){
+	var scrollTopAtual = $(this).scrollTop();
+
+	if (scrollTopAtual > 200) {
+		$('#navegacao').addClass('fixed');
+	}else{
+		$('#navegacao').removeClass('fixed');
+	}
 })
